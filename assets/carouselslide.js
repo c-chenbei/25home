858710f761d26a3cf814fde1b4a6cfc115174d66
c-chenbei <563540292@qq.com>
2023-10-23 17:17:12 +0800

    清空模板

--- assets/carouselslide.js
+++ assets/carouselslide.js
@@ -16,6 +16,7 @@ function updateCountdown() {
     console.log(targetDate,now,'55555555')
     if (timeRemaining <= 0) {
         window.clearTimeout(timers);
+        document.getElementById("countdowns_milliseconds").textContent = '0'
     } else {
         const totalHours = Math.floor(timeRemaining / (1000 * 60 * 60));
         // const days = Math.floor(totalHours / 24); // 将小时转换为天
